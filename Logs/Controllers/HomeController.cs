e410f973bdffe58011837bc78a2045b790f73a60
Jim Getty <jgett@umich.edu>
2017-03-08 10:27:58 -0500

    Initial commit.

---
+++ Logs/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Configuration;
+using System.Threading.Tasks;
+using Logs.Models;
+
+namespace Logs.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("autoend")]
+        public async Task<ActionResult> AutoEnd()
+        {
+            MongoClient mongo = new MongoClient(ConfigurationManager.AppSettings["MongoConnectionString"]);
+            var db = mongo.GetDatabase("logs");
+            var col = db.GetCollection<BsonDocument>("autoend");
+            var docs = await col.Find(FilterDefinition<BsonDocument>.Empty).ToListAsync();
+            var model = docs.Select(AutoEndModel.Create).ToList();
+            return View(model);
+        }
+    }
+}
---
+++ Logs/Models/AutoEndModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MongoDB.Bson;
+
+namespace Logs.Models
+{
+    public class AutoEndModel
+    {
+        public int ReservationID { get; set; }
+        public int ResourceID { get; set; }
+        public string ResourceName { get; set; }
+        public int ClientID { get; set; }
+        public string DisplayName { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public static AutoEndModel Create(BsonDocument doc)
+        {
+            return new AutoEndModel()
+            {
+                ReservationID = doc["ReservationID"].AsInt32,
+                ResourceID = doc["ResourceID"].AsInt32,
+                ResourceName = doc["ResourceName"].AsString,
+                ClientID = doc["ClientID"].AsInt32,
+                DisplayName = doc["DisplayName"].AsString,
+                Timestamp = doc["Timestamp"].ToUniversalTime().ToLocalTime()
+            };
+            
+        }
+    }
+}
